72680d0b194f7b92dc582c5ae8322ddab5908328
messaismael <messaismael01@gmail.com>
2022-03-06 21:35:47 +0100

    add conctact class

---
+++ Data/Contact.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp.Data;
+
+public class Contact
+{
+    
+    public DateTime Date {get; set;}
+    public string? Name {get; set;}
+    public int Number {get; set;}
+    public String? Email {get; set;}
+}
---
+++ Data/ContactService.cs
@@ -0,0 +1,19 @@
+namespace BlazorApp.Data;
+
+public class ContactService
+{
+    public void AddContat(Contact cnt)
+    {
+
+    }
+
+    public void DeleteContact(Contact cnt)
+    {
+
+    }
+
+    public void UpdateContact(Contact cnt)
+    {
+
+    }
+}
